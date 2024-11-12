94cf4a622571be8639cccdbb287787270855a06d
Heitor Rosa <heitorrs.dev@gmail.com>
2024-11-12 15:40:08 -0300

    chore: added spaces

--- Hortifruti do Sr. Elias/funcoes.h
+++ Hortifruti do Sr. Elias/funcoes.h
@@ -9,18 +9,31 @@ extern std::vector<Produto> produtos;
 extern std::vector<Fornecedor> fornecedores;
 
 void limparTerminal();
+
 Produto criarProduto(int id, const std::vector<Fornecedor>& fornecedores);
+
 void salvarProdutos(const std::vector<Produto>& produtos);
+
 void salvarFornecedores(const std::vector<Fornecedor>& fornecedores);
+
 void adicionarProduto(std::vector<Produto>& produtos, const std::vector<Fornecedor>& fornecedores);
+
 void editarProduto(std::vector<Produto>& produtos, const std::vector<Fornecedor>& fornecedores);
+
 void listarProdutos(const std::vector<Produto>& produtos, const std::vector<Fornecedor>& fornecedores);
+
 void listarFornecedores(const std::vector<Fornecedor>& fornecedores);
+
 Fornecedor criarFornecedor(int id);
+
 void adicionarFornecedor(std::vector<Fornecedor>& fornecedores);
+
 void carregarProdutos(std::vector<Produto>& produtos);
+
 void carregarFornecedores(std::vector<Fornecedor>& fornecedores);
+
 void calcularCompra(std::vector<Produto>& produtos);
+
 void menuPrograma();
 
 #endif // FUNCOES_H
